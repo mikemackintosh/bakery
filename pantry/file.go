ad601055e628d48d0c25ecbc978d3a8782a97abe
Mike Mackintosh <m@zyp.io>
2019-02-04 20:34:30 -0800

    start adding file resource

---
+++ pantry/file.go
@@ -0,0 +1,13 @@
+package pantry
+
+import "os"
+
+// FileExists detects if a file exists on the filesystem or not
+func FileExists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
