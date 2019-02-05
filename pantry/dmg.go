f1bf74cf4f8c89a77fdfd63205c63039b45c7825
Mike Mackintosh <m@zyp.io>
2019-02-04 20:33:30 -0800

    fix destination command argument

--- pantry/dmg.go
+++ pantry/dmg.go
@@ -3,6 +3,7 @@ package pantry
 import (
 	"fmt"
 	"net/url"
+	"path"
 	"strings"
 
 	"github.com/hashicorp/hcl2/hcl"
@@ -12,15 +13,16 @@ import (
 	"github.com/zclconf/go-cty/cty"
 )
 
+// Dmg is a MacOS DMG object
 type Dmg struct {
 	PantryItem
 	Name           string   `hcl:"name,label"`
 	Config         hcl.Body `hcl:",remain"`
 	AcceptEula     bool     `json:"accept_eula"`
 	AllowUntrusted bool     `json:"allow_untrusted"`
-	Checksum       string   `json:"checksum"`
+	Checksum       *string  `json:"checksum"`
 	App            *string  `json:"app"`
-	Destination    string   `json:"destination"`
+	Destination    *string  `json:"destination"`
 	Source         string   `json:"source"`
 	DependsOn      []string `json:"depends_on"`
 }
@@ -60,6 +62,20 @@ var dmgSpec = &hcldec.ObjectSpec{
 	},
 }
 
+// GetDestination will get or return the default destination
+func (d *Dmg) GetDestination() string {
+	if d.Destination != nil {
+		destination := *d.Destination
+		if string(destination[len(destination)-1]) != "/" {
+			destination = destination + "/"
+		}
+		return destination
+	}
+
+	return "/Applications/"
+}
+
+// Parse will parse the config with the spec
 func (d *Dmg) Parse(evalContext *hcl.EvalContext) error {
 	cli.Debug(cli.INFO, "Preparing DMG", d.Name)
 	cfg, diags := hcldec.Decode(d.Config, dmgSpec, evalContext)
@@ -78,7 +94,16 @@ func (d *Dmg) Parse(evalContext *hcl.EvalContext) error {
 	return nil
 }
 
+// Bake will perform the DMG installation
 func (d *Dmg) Bake() {
+	// Rsync the app from the mounted DMG to the destination folder
+	var appName = d.Name
+	if d.App != nil {
+		appName = *d.App
+	}
+	// Sets the app name with the .app extension
+	var appNameWithExt = appName + ".app"
+
 	u, err := url.Parse(d.Source)
 	if err != nil {
 		cli.Debug(cli.INFO, fmt.Sprintf("Error finding source %s", d.Source), err)
@@ -88,41 +113,38 @@ func (d *Dmg) Bake() {
 	if u.Scheme == "http" || u.Scheme == "https" {
 		cli.Debug(cli.DEBUG, "\t-> Using HTTP(s) source for download", nil)
 
-		urlParse, err := url.Parse(d.Source)
+		urlParse, urlErr := url.Parse(d.Source)
 		if err != nil {
-			cli.Debug(cli.INFO, fmt.Sprintf("Error finding source %s", d.Source), err)
+			cli.Debug(cli.INFO, fmt.Sprintf("Error finding source %s", d.Source), urlErr)
 		}
 
-		tmpFile = config.Registry.TempDir + urlParse.Path
-		err = DownloadFile(d.Source, tmpFile, d.Checksum)
-		if err != nil {
-			cli.Debug(cli.INFO, fmt.Sprintf("Error downloading file %s", d.Source), err)
+		tmpFile = config.Registry.TempDir + "/" + path.Base(urlParse.Path)
+		urlErr = DownloadFile(d.Source, tmpFile, *d.Checksum)
+		if urlErr != nil {
+			cli.Debug(cli.INFO, fmt.Sprintf("Error downloading file %s", d.Source), urlErr)
 		}
 	}
 
 	// Mount it
 	var mountpoint = fmt.Sprintf("/Volumes/%s", d.Name)
-	cli.Debug(cli.INFO, fmt.Sprintf("Mounting %s", tmpFile), err)
-	hdiutilBinary := "/usr/bin/hdiutil"
-	o, err := RunCommand([]string{
+	var hdiutilBinary = "/usr/bin/hdiutil"
+	var mountCmd = []string{
 		hdiutilBinary,
 		"attach",
 		strings.Replace(tmpFile, " ", "\\ ", -1),
 		"-nobrowse",
 		"-mountpoint",
-		mountpoint})
+		mountpoint}
+	cli.Debug(cli.INFO, fmt.Sprintf("Mounting %s", tmpFile), err)
+	cli.Debug(cli.DEBUG2, fmt.Sprintf("\t-> Install command: %#v", strings.Join(mountCmd, " ")), err)
+	r, err := RunCommand(mountCmd)
 	if err != nil {
 		cli.Debug(cli.ERROR, fmt.Sprintf("Error mounting %s to %s", tmpFile, mountpoint), err)
-		cli.Debug(cli.ERROR, "a", o.Raw)
 	}
+	cli.Debug(cli.DEBUG2, fmt.Sprintf("\t-> Mount command response: \n%s", r.FormattedString()), err)
 
-	// Rsync the app from the mounted DMG to the destination folder
-	var appName = d.Name
-	if d.App != nil {
-		appName = *d.App
-	}
-	cli.Debug(cli.INFO, fmt.Sprintf("Installing %s to %s", tmpFile, d.Destination), err)
-	_, err = RunCommand([]string{
+	var installCmd = []string{
+		"sudo",
 		"/usr/bin/rsync",
 		"--force",
 		"--recursive",
@@ -132,11 +154,15 @@ func (d *Dmg) Bake() {
 		"--owner",
 		"--group",
 		"--times",
-		fmt.Sprintf("%s/%s.app", mountpoint, appName),
-		d.Destination})
+		fmt.Sprintf("%s/%s", mountpoint, appNameWithExt),
+		d.GetDestination()}
+	cli.Debug(cli.INFO, fmt.Sprintf("Installing %s to %s", tmpFile, d.GetDestination()), err)
+	cli.Debug(cli.DEBUG2, fmt.Sprintf("\t-> Install command: %s", strings.Join(installCmd, " ")), err)
+	r, err = RunCommand(installCmd)
 	if err != nil {
-		cli.Debug(cli.INFO, fmt.Sprintf("Error downloading file %s", d.Source), err)
+		cli.Debug(cli.INFO, fmt.Sprintf("Error installing %s", appName), err)
 	}
+	cli.Debug(cli.DEBUG2, fmt.Sprintf("\t-> Install command response: \n%s", r.FormattedString()), err)
 
 	// unmount the DMG after copying it over
 	_, err = RunCommand([]string{hdiutilBinary, "unmount", mountpoint})
