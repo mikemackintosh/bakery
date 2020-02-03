bac04c8b61b51a331f26e75c3903f8c90a7767ba
Mike Mackintosh <m@zyp.io>
2020-02-03 15:29:25 -0800

    update download test

--- pantry/download_test.go
+++ pantry/download_test.go
@@ -1,8 +1,10 @@
 package pantry
 
 import (
-	"testing"
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 var testDownloadFile = []struct {
@@ -11,21 +13,27 @@ var testDownloadFile = []struct {
 	Checksum    string
 }{
 	{
-		Source:      "http://localhost:3000/test_download.txt",
+		Source:      "/test_download.txt",
 		Destination: "/tmp/test_download.txt",
 		Checksum:    "241fc234ac0c3cd1d85cd766800a72dc79a46f62fc29e99b1b3e2a41bfeece2b",
 	},
 }
 
 func TestDownloadFile(t *testing.T) {
-		http.Handle("/test_download.txt", http.FileServer(http.Dir("../testing/fixtures/www")))
-		go http.ListenAndServe("localhost:3000", nil)
-
-		for _, test := range testDownloadFile {
-			err := DownloadFile(test.Source, test.Destination, test.Checksum)
-			if err != nil {
-				t.Errorf("DownloadFile failed with %s", err)
-			}
+	var filename = "/test_download.txt"
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" && r.URL.Path == filename {
+			b, _ := ioutil.ReadFile("../../testing/fixtures/www/test_download.txt")
+			_, _ = w.Write(b)
+			return
 		}
+	}))
+
+	for _, test := range testDownloadFile {
+		err := DownloadFile(ts.Listener.Addr().String()+test.Source, test.Destination, test.Checksum)
+		if err != nil {
+			t.Errorf("DownloadFile failed with %s", err)
+		}
+	}
 
 }
